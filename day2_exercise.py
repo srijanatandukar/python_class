5b27f03ca6859f6dd9927f505f7e7b2916db7b8b
srijana <tandukar2002@gmail.com>
2024-08-02 20:30:42 +0545

    Revision

---
+++ day2_exercise.py
@@ -0,0 +1,21 @@
+#Exercise - Day 3 
+# age = int(22)
+# height = float(4.11)
+# complex_num = complex(5 + 3j) 
+# print(type(complex_num))
+# b = float(input("Enter the base of the triagle : "))
+# h = float(input("Enter the height of the triangle: "))
+# area = 0.5 * b * h
+# print(f"The area of the triangle is {area}")
+
+# a= int(input("Enter a side of triangle: "))
+# b= int(input("Enter another side: "))
+# c = int(input("Enter the third side of the triangle: "))
+# perimeter = a + b + c
+# print(f"The perimeter of the triangle is {perimeter}")
+
+rad = int(input("Enter the radius: "))
+pi = 3.14
+area = int(pi * rad ** 2)
+circumference= float(2 * pi * rad)
+print(f"The area of circle is {area} and circumference is {circumference:.3f}")
